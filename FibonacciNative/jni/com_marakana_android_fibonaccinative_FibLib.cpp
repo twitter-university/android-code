78c2865d6419edfc7772ce9397e405e618632bab
Aleksandar Gargenta <sasa@gargenta.com>
2012-02-27 00:43:20 -0600

    Initial commit

---
+++ FibonacciNative/jni/com_marakana_android_fibonaccinative_FibLib.cpp
@@ -0,0 +1,68 @@
+#include "jni.h"
+
+/***********************************************************
+ * This is an alternative implementation that demonstrates *
+ * how to pre-link native funcitons to the managed JNI     *
+ * environment.                                            *
+ * To test, switch to this file in Android.mk              *
+ ***********************************************************/
+
+#ifndef NELEM
+# define NELEM(x) ((int) (sizeof(x) / sizeof((x)[0])))
+#endif
+
+namespace com_marakana_android_fibonaccinative {
+
+    static jlong fib(jlong n) {
+        return n <= 0 ? 0 : n == 1 ? 1 : fib(n - 1) + fib(n - 2);
+    }
+
+    static jlong fibNR(JNIEnv *env, jclass clazz, jlong n) {
+        return fib(n);
+    }
+
+    static jlong fibNI(JNIEnv *env, jclass clazz, jlong n) {
+        jlong previous = -1;
+        jlong result = 1;
+        jlong i;
+        for (i = 0; i <= n; i++) {
+            jlong sum = result + previous;
+            previous = result;
+            result = sum;
+        }
+        return result;
+    }
+
+    static JNINativeMethod method_table[] = {
+        { "fibNR", "(J)J", (void*)fibNR },
+        { "fibNI", "(J)J", (void*)fibNI }
+    };
+}
+
+using namespace com_marakana_android_fibonaccinative;
+
+static int jniRegisterNativeMethods(JNIEnv* env, const char* className,
+        const JNINativeMethod* gMethods, int numMethods) {
+    jclass clazz = env->FindClass(className);
+    if (clazz) {
+        env->RegisterNatives(clazz, gMethods, numMethods);
+        env->DeleteLocalRef(clazz);
+        return 0;
+    } else {
+        return -1;
+    }
+}
+
+extern "C" jint JNI_OnLoad(JavaVM* vm, void* reserved) {
+    JNIEnv* env;
+    if (vm->GetEnv(reinterpret_cast<void**>(&env), JNI_VERSION_1_6) != JNI_OK) {
+        return -1;
+    } else {
+        if (jniRegisterNativeMethods(env, "com/marakana/android/fibonaccinative/FibLib",
+                method_table, NELEM(method_table)) != 0) {
+            return -2;
+        }
+    }
+    return JNI_VERSION_1_6;
+}
+
